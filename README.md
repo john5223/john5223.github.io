119cc8831f2424bebdb0eb02e39b537f8251b57f
John Curran <curran736@gmail.com>
2025-09-11 00:36:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Prerequisites
+
+
+
+# Run local
+
+```
+npm install
+npm run dev
+```
+
+
+Based on https://github.com/RyanFitzgerald/devportfolio?tab=readme-ov-file
---
+++ src/config.ts
@@ -0,0 +1,43 @@
+export const siteConfig = {
+  name: "John Curran",
+  title: "Senior Software Engineer",
+  description: "Portfolio website of John Curran",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "curran736@gmail.com",
+    linkedin: "https://www.linkedin.com/in/john-curran-77957b39",
+    github: "https://github.com/john5223",
+  },
+  aboutMe:
+    "I am a senior software engineer currently looking for a new role.",
+  skills: ["Kubernetes", "AWS", "Python", "Docker", "Javascript", "React", "Node.js",],
+  projects: [
+    {
+      name: "Candle stick pattern analysis",
+      description:
+        "Analyze candle stick patterns in the stock market to look for reliable patterns to predict future market trends.",
+      link: "https://john5223.github.io/patterns",
+      skills: ["Python", "Pandas", "Numpy"],
+    },
+  ],
+  experience: [
+    {
+      company: "Red Knight",
+      title: "DevSecOps Engineer",
+      dateRange: "July 2023 - August 2025",
+      bullets: [
+        "",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "Texas State University",
+      degree: "Bachelor of Science in Computer Science",
+      dateRange: "2007 - 2011",
+      achievements: [
+        "President of Computer Science Club",
+      ],
+    },
+  ],
+};
